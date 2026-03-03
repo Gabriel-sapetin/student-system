6cf75476793fbb11a85fffb044480b65551190b6
gabrielsapetin <alfredjacob1205@gmail.com>
2026-03-03 17:02:37 +0800

    Updated app

--- py1.py
+++ py1.py
@@ -90,8 +90,9 @@ def delete_group():
     save_data(groups)
     return jsonify({'status': 'deleted'})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+import os
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(debug=True, host='0.0.0.0', port=port)
+
